e540bcfcaf25cb8a56e4a2bc3162de58b1030d43
smnra <smnra@163.com>
2018-05-25 12:06:45 +0800

    my Solution
     and The Best Solution!

--- Simple Encryption #1 - Alternating Split.py
+++ Simple Encryption #1 - Alternating Split.py
@@ -99,5 +99,23 @@ print(decrypt("This is a test!", 4), "This is a test!")
 
 
 
+'''
+# The Best Suloation!
 
+def decrypt(s, n):
+    if not s: return s
+    o, l = len(s) // 2, list(s)
+    for _ in range(n):
+        l[1::2], l[::2] = l[:o], l[o:]
+    return ''.join(l)
+
+def encrypt(s, n):
+    if not s: return s
+    for _ in range(n):
+        s = s[1::2] + s[::2]
+    return s
+
+
+
+'''
 
